548ef79e7ad4247fe36248b37a14767ca9bcb601
zhouwg <zhouwg2000@gmail.com>
2025-07-08 10:23:56 +0800

    project: sync with upstream(PR-14563)

--- scripts/build-run-ggmlhexagon-android.sh
+++ scripts/build-run-ggmlhexagon-android.sh
@@ -74,11 +74,13 @@ TEST_MODEL_NAME=/sdcard/gemma-3n-E4B-it-Q8_0.gguf
 #for llama-cli, 3.9 GiB
 #TEST_MODEL_NAME=/sdcard/gemma-3-4b-it-Q8_0.gguf
 #for llama-cli, 1.6 GiB, can be downloadded via this script in function check_prebuilt_models()
-TEST_MODEL_NAME=/sdcard/MiniCPM4-0.5B-F32.gguf
-#for llama-cli, 1.1 GiB, will be downloaded automatically via this script when running this script at the first time
-TEST_MODEL_NAME=/sdcard/qwen1_5-1_8b-chat-q4_0.gguf
+#TEST_MODEL_NAME=/sdcard/MiniCPM4-0.5B-F32.gguf
 #for llama-cli, 4.5 GiB, will be downloadded automatically via this script when running this script at the first time
 #TEST_MODEL_NAME=/sdcard/gemma-3n-E2B-it-Q8_0.gguf
+#for llama-cli, 1.1 GiB, will be downloaded automatically via this script when running this script at the first time
+#this model will be used for compare performance/stability between ggml-opencl, ggml-vulkan, ggml-hexagon on Snapdragon 8Elite
+#because the weights of this model is suitable for ggml-opencl,ggml-vulkan,ggml-hexagon on Android phone
+TEST_MODEL_NAME=/sdcard/qwen1_5-1_8b-chat-q4_0.gguf
 
 #for llama-bench, 4.5 GiB, will be downloadded automatically via this script when running this script at the first time
 GGUF_MODEL_NAME=/sdcard/gemma-3n-E2B-it-Q8_0.gguf
