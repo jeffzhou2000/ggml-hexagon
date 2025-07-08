128b6e41eb63fd772ef2032fb5d18568ed4949d9
zhouwg <zhouwg2000@gmail.com>
2025-07-08 17:16:49 +0800

    script: fix a minor issue in aaptest.sh

--- scripts/aaptest.sh
+++ scripts/aaptest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2025 The KanTV authors
+# Copyright (c) 2025 zhouwg(https://github.com/zhouwg)
 #
 # Accuracy And Performance test of various mulmat algotype on Hexagon-cDSP
 #
@@ -10,10 +10,11 @@ PWD=`pwd`
 PROJECT_HOME_PATH=`pwd`
 PROJECT_ROOT_PATH=${PROJECT_HOME_PATH}
 
-algo_types="0 1 2 3 4 5 6 32 33"
+#algo_types="0 1 2 3 4 5 6 32 33"
+algo_types="0 1 2 3 4 5 6 32"
 
 for algo in $algo_types
 do
     ${PROJECT_ROOT_PATH}/scripts/build-run-android.sh run_benchmark MUL_MAT 3 ${algo}
-    ${PROJECT_ROOT_PATH}/scripts/build-run-android.sh run_testop    MUL_MAT   ${algo}
+    ${PROJECT_ROOT_PATH}/scripts/build-run-android.sh run_testop    MUL_MAT 3 ${algo}
 done
